65fbbc7ddbef303e975dd068cbe09a24577c0d8b
Maurice Gagarin <Maurice Gagarin@LAPTOP-S9BQTC7M>
2024-02-28 23:34:44 +0800

    Add project files.

---
+++ PRELIM_E3_Assignment_GagarinMaurice_BSCS32E1/Pages/School.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRELIM_E3_Assignment_GagarinMaurice_BSCS32E1.Pages
+{
+    public class School : PageModel
+    {
+        private readonly ILogger<School> _logger;
+
+        public School(ILogger<School> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
